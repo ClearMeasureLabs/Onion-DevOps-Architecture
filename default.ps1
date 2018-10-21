5dffb0fa546bab8e9c3109e042df0108cbbc2329
Jeffrey Palermo <jeffrey@clear-measure.com>
2018-10-21 17:34:06 -0500

    Consolidated build script with dotnet command line switches Cleaned up some files committed by accident Redirected build and test output to .\build

--- default.ps1
+++ default.ps1
@@ -5,13 +5,12 @@ properties {
     $base_dir = resolve-path .\
     $source_dir = "$base_dir\src"
     $unitTestProjectPath = "$source_dir\UnitTests\UnitTests.csproj"
-	$testCopyIgnorePath = ".vs"
 	$projectConfig = $env:Configuration
     $version = $env:Version
-    $nunitPath = Resolve-Path("$source_dir\packages\nunit.consolerunner\*\tools\")
 	$verbosity = "m"
 
     $build_dir = "$base_dir\build"
+	$test_dir = "$build_dir\test"
     
     if ([string]::IsNullOrEmpty($version)) { $version = "1.0.0"}
     if ([string]::IsNullOrEmpty($projectConfig)) {$projectConfig = "Release"}
@@ -21,7 +20,7 @@ task default -depends Init, Compile, Test
 
 task Init {
     rd $build_dir -recurse -force  -ErrorAction Ignore
-	md $build_dir > nil
+	md $build_dir > $null
 
 	exec {
 		& dotnet clean $source_dir\$projectName.sln -nologo -v $verbosity
@@ -37,13 +36,13 @@ task Init {
 
 task Compile -depends Init {
 	exec {
-		& dotnet build $source_dir\$projectName.sln -nologo -restore:False -v $verbosity -maxcpucount -p:Configuration=$projectConfig 
+		& dotnet build $source_dir\$projectName.sln -nologo --no-restore -v $verbosity -maxcpucount --configuration $projectConfig --no-incremental --output $build_dir
 	}
 }
 
 task Test -depends Compile {
 	exec {
-		& dotnet test $unitTestProjectPath -nologo -v $verbosity --logger:trx --results-directory $build_dir --no-build -p:Configuration=$projectConfig 
+		& dotnet test $unitTestProjectPath -nologo -v $verbosity --logger:trx --results-directory $test_dir --no-build --no-restore --configuration $projectConfig
 		}
     
 }  
