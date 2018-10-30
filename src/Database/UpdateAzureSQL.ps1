f5b7dd14a2fdc7e5253cf0eb730841c071df1dcb
Jeffrey Palermo <jeffrey@clear-measure.com>
2018-10-30 17:25:12 -0500

    #10 troubleshooting deployment for database

--- src/Database/UpdateAzureSQL.ps1
+++ src/Database/UpdateAzureSQL.ps1
@@ -7,6 +7,9 @@ $DatabaseAction = $env:DatabaseAction
 $DatabaseUser = $env:DatabaseUser
 $DatabasePassword = $env:DatabasePassword
 
+Write-Output "Recursive directory listing for diagnostics"
+Get-ChildItem -Recurse
+
 Write-Host "Executing & .\scripts\AliaSQL.exe $DatabaseAction $databaseServer $databaseName .\scripts $databaseUser $databasePassword"
 
 & .\scripts\AliaSQL.exe $DatabaseAction $DatabaseServer $DatabaseName .\scripts $DatabaseUser $DatabasePassword
