6f990d1b5d1dd2b914593d65250f9bf24cb7adc4
Jeffrey Palermo <jeffrey@clear-measure.com>
2018-10-30 20:33:27 -0500

    #11 working on website deployment

---
+++ src/UI/AzureConnection.ps1
@@ -0,0 +1,29 @@
+#
+# AzureConnection.ps1
+#
+$path = $PSScriptRoot
+$configFile = "$path\ConnectionStrings.config"
+$databaseName = $env:DatabaseName
+$databaseServer = $env:DatabaseServer
+$databaseUser = $env:DatabaseUser
+$databasePassword = $env:DatabasePassword
+$connection_string = "Server=tcp:$databaseServer,1433;Initial Catalog=$databaseName;Persist Security Info=False;User ID=$databaseUser;Password=$databasePassword;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+
+$xpath = "//add[@name='Bootcamp']/@connectionString"
+$filePath = Resolve-Path $configFile
+$value = $connection_string
+
+[xml] $fileXml = Get-Content $filePath
+$node = $fileXml.SelectSingleNode($xpath)
+        
+if($node.NodeType -eq "Element") {
+    $node.InnerText = $value
+} else {
+    $node.Value = $value
+}
+
+$fileXml.Save($filePath) 
+
+Write-Host "DatabaseServer: $DatabaseServer"
+Write-Host "DatabaseName: $DatabaseName"
+Write-Host "-----------------------"
