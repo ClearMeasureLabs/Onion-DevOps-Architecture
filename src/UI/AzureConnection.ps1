119912dfb91663ac138f99d7cc3427623d9b2cac
Jeffrey Palermo <jeffrey@clear-measure.com>
2018-10-30 20:49:05 -0500

    #11 Fixing path to ConnectionStrings.config

--- src/UI/AzureConnection.ps1
+++ src/UI/AzureConnection.ps1
@@ -2,14 +2,14 @@
 # AzureConnection.ps1
 #
 $path = $PSScriptRoot
-$configFile = "$path\ConnectionStrings.config"
+$configFile = "$path\bin\Release\netcoreapp2.1\ConnectionStrings.config"
 $databaseName = $env:DatabaseName
 $databaseServer = $env:DatabaseServer
 $databaseUser = $env:DatabaseUser
 $databasePassword = $env:DatabasePassword
 $connection_string = "Server=tcp:$databaseServer,1433;Initial Catalog=$databaseName;Persist Security Info=False;User ID=$databaseUser;Password=$databasePassword;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
 
-$xpath = "//add[@name='Bootcamp']/@connectionString"
+$xpath = "//add[@name='Database']/@connectionString"
 $filePath = Resolve-Path $configFile
 $value = $connection_string
 
